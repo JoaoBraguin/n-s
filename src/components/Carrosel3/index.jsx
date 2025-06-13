00feb124801290d239a9474f58f47f4de9c471b4
JoaoBraguin <joaopedromoraes76@gmail.com>
2025-06-13 07:32:55 -0300

    atualizacao

--- src/components/Carrosel3/index.jsx
+++ src/components/Carrosel3/index.jsx
@@ -20,7 +20,7 @@ export default function Carrosel3({ titulo, videos }) {
 
   return (
     <div className={style.secaoCarrossel}>
-      <iframe className={style.iframe1} onPlay={true} src="https://open.spotify.com/embed/track/59qC8XPUyBWsmr9v3CQPGF?utm_source=generator" width="80%" height="152" frameBorder="0" allowFullScreen allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" ></iframe>
+      <iframe className={style.iframe1} onPlay={true} src="https://open.spotify.com/embed/track/6NgW7wzdmgLJ81XPZMoTI6?utm_source=generator" width="80%" height="152" frameBorder="0" allowFullScreen allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" ></iframe>
       <div className={style.carrossel}>
         <div className={style.carrosselItens}>
           <div className={style.item}>
@@ -33,15 +33,18 @@ export default function Carrosel3({ titulo, videos }) {
         </div>
         <div className={style.eu}>
             <strong>Eu te amo há:</strong>
-            <h3>1 ano, 10 meses</h3>
+            <h3>2 anos, 1 mês</h3>
             <strong className={style.linha}>
               _______________________________
             </strong>
             <div className={style.dec}>
-              <h2>Cada dia que passa meu coração transborda de amor por você.</h2>
-              <h2>Cada sorriso seu me enche de alegria e ilumina meu dia, meu mundo.</h2>
+              <h1 className={style.feliz}>FELIZ DIA DOS NAMORADOS MEUUUU AMOORRRRR❤</h1>
+              <h2>Obrigado por ser essa mulher incrivel na minha vida!</h2>
+              <h2>Por me amar, cuidar de mim, cada sorriso seu me alagra a cada dia mais.</h2>
               <h2>Seu Beijo seu Abraço me fazem ter certeza de que é você que eu quero pra minha vida, sempre ficar ao seu lado, enfrentando todos os problemas juntos e superando todos eles para ficarmos juntos.</h2>
               <h2>Muito Obrigado por estar ao meu lado, cada dia você me inspira a ser uma pessoa melhor.</h2>
+              <h2>Quero ficar com voce pra sempre!</h2>
+              <h1>PRA SEMPRE EU E VOCE!❤</h1>
               <h1 className={style.final}>EU te amo para sempre ! ❤</h1>
               
               
