16becf3408a4a47d10443b4c140d9dbf73630083
JoaoBraguin <joaopedromoraes76@gmail.com>
2026-05-21 13:00:50 -0300

    correção fotos

--- src/App.jsx
+++ src/App.jsx
@@ -6,9 +6,9 @@ import foto1 from './images/foto1.jpeg';
 import foto2 from './images/foto2.jpeg';
 import foto3 from './images/foto3.jpeg';
 import foto4 from './images/foto4.jpeg';
-import foto5 from './images/foto5.jpeg';
+// import foto5 from './images/foto5.jpeg';
 // import foto6 from './images/foto6.jpeg';
-import foto7 from './images/foto7.jpeg';
+// import foto7 from './images/foto7.jpeg';
 import foto8 from './images/foto8.jpeg';
 
 function App() {
@@ -36,9 +36,9 @@ function App() {
     { src: foto2, title: "", description: "" },
     { src: foto3, title: "", description: "" },
     { src: foto4, title: "", description: "" },
-    { src: foto5, title: "", description: "" },
+    // { src: foto5, title: "", description: "" },
     // { src: foto6, title: "", description: "" },
-    { src: foto7, title: "", description: "" },
+    // { src: foto7, title: "", description: "" },
     { src: foto8, title: "", description: "" },
   ];
 
