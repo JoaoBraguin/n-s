1ca3a600233f9dc2a052cfe88231d75cf7b07612
JoaoBraguin <joaopedromoraes76@gmail.com>
2026-05-21 12:55:34 -0300

    correção iframe

--- src/components/Carrosel3/index.jsx
+++ src/components/Carrosel3/index.jsx
@@ -23,6 +23,7 @@ export default function Carrosel3({ titulo, videos }) {
                 frameBorder="0"
                 allowFullScreen
                 allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+                title="Spotify music player"
             />
 
 
